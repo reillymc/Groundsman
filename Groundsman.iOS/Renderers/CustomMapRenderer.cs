9c9b121ab1848b8becea77f5712d15936836f487
ReillyM-C <reilly@mackenzie-cree.net>
2020-07-26 00:45:10 +1000

    Improve iOS native map UI and update build information

--- Groundsman.iOS/Renderers/CustomMapRenderer.cs
+++ Groundsman.iOS/Renderers/CustomMapRenderer.cs
@@ -13,6 +13,7 @@ namespace Groundsman.iOS.Renderers
 {
     public class CustomMapRenderer : MapRenderer
     {
+        UIColor GroundsmanGreen = new UIColor(red: 0.30f, green: 0.69f, blue: 0.31f, alpha: 1.00f);
         protected override void OnElementChanged(ElementChangedEventArgs<View> e)
         {
             base.OnElementChanged(e);
@@ -21,36 +22,71 @@ namespace Groundsman.iOS.Renderers
             {
                 MKMapView nativeMap = Control as MKMapView;
                 nativeMap.ShowsCompass = false;
+                nativeMap.TintColor = GroundsmanGreen;
                 AddUserTrackingButton(nativeMap);
             }
         }
 
         private void AddUserTrackingButton(MKMapView nativeMap)
         {
-            MKUserTrackingButton button = MKUserTrackingButton.FromMapView(nativeMap);
-            button.Layer.BackgroundColor = UIColor.White.ColorWithAlpha(0.9F).CGColor;
-            button.Layer.BorderColor = UIColor.White.ColorWithAlpha(0.9F).CGColor;
-            button.Layer.ShadowColor = UIColor.Black.CGColor;
-            button.Layer.ShadowOpacity = 0.3F;
-            button.Layer.ShadowRadius = 4;
-            button.Layer.BorderWidth = 1;
-            button.Layer.CornerRadius = 4;
-            button.Layer.MasksToBounds = false;
-            button.TranslatesAutoresizingMaskIntoConstraints = false;
+            // Tracking button
+            MKUserTrackingBarButtonItem trackingButton = new MKUserTrackingBarButtonItem(nativeMap);
+            trackingButton.CustomView.TintColor = GroundsmanGreen;
+            trackingButton.CustomView.Frame = new CGRect(x: trackingButton.CustomView.Frame.X, y: trackingButton.CustomView.Frame.Y, width: 44, height: 44);
 
+            // Toolbar and frame
+            var toolBarFrame = new CGRect(location: new CGPoint(x: 0, y: 0), size: new CGSize(width: 44, height: 44));
+            var toolbar = new UIToolbar(frame: toolBarFrame)
+            {
+                BarTintColor = UIColor.SystemBackgroundColor,
+                Translucent = true
+            };
+
+            UIBarButtonItem flex = new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace, target: this, action: null);
+
+            // Set toolbar items - can be expanded later
+            toolbar.Items = new UIBarButtonItem[] { flex, trackingButton, flex };
+
+            // Set toolbar position
+            var origin = new CGPoint(x: UIScreen.MainScreen.Bounds.Width - 53, y: 52);
+
+            // Create base view for button
+            UIView baseView = new UIView(frame: new CGRect(location: origin, size: new CGSize(width: 44, height: 44)))
+            {
+              BackgroundColor = UIColor.Clear
+            };
+
+            // Shadow effects
+            baseView.Layer.ShadowColor = UIColor.Black.CGColor;
+            baseView.Layer.ShadowOpacity = 0.3f;
+            baseView.Layer.ShadowOffset = new CGSize(width: 0, height: 0);
+            baseView.Layer.ShadowRadius = 4.0f;
+
+            // Improves performance but causes flickering and artefacts
+            //baseView.Layer.ShouldRasterize = true;
+            //baseView.Layer.RasterizationScale = UIScreen.MainScreen.Scale;
+
+            // Rounded border view for button
+            UIView borderview = new UIView();
+            borderview.Frame = baseView.Bounds;
+            borderview.Layer.CornerRadius = 10;
+            borderview.Layer.MasksToBounds = true;
+
+            // Stack subviews
+            baseView.AddSubview(borderview);
+            borderview.AddSubview(toolbar);
+            nativeMap.AddSubview(baseView);
+
+            // Compass button
             MKCompassButton compass = MKCompassButton.FromMapView(nativeMap);
             compass.CompassVisibility = MKFeatureVisibility.Visible;
-            compass.Frame = new CGRect(new CGPoint(45 , 45), compass.Frame.Size);
+            compass.Frame = new CGRect(new CGPoint(45, 45), compass.Frame.Size);
             compass.TranslatesAutoresizingMaskIntoConstraints = false;
 
-            nativeMap.AddSubview(button);
             nativeMap.AddSubview(compass);
 
-            button.TopAnchor.ConstraintEqualTo(nativeMap.TopAnchor, 50).Active = true;
-            button.RightAnchor.ConstraintEqualTo(nativeMap.RightAnchor, -4).Active = true;
-
-            compass.TopAnchor.ConstraintEqualTo(nativeMap.TopAnchor, 100).Active = true;
-            compass.RightAnchor.ConstraintEqualTo(nativeMap.RightAnchor, -6).Active = true;
+            compass.TopAnchor.ConstraintEqualTo(nativeMap.TopAnchor, 108).Active = true;
+            compass.RightAnchor.ConstraintEqualTo(nativeMap.RightAnchor, -14).Active = true;
         }
     }
 }
