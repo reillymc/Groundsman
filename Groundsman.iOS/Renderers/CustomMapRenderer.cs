bd1ea76fa17d47cf8d214e2b5829eabf39b54572
ReillyM-C <reilly@mackenzie-cree.net>
2020-07-26 13:30:01 +1000

    Fixed crash on iOS12 with custom map button colour

--- Groundsman.iOS/Renderers/CustomMapRenderer.cs
+++ Groundsman.iOS/Renderers/CustomMapRenderer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Groundsman;
 using Groundsman.iOS.Renderers;
 using MapKit;
@@ -13,7 +12,7 @@ namespace Groundsman.iOS.Renderers
 {
     public class CustomMapRenderer : MapRenderer
     {
-        UIColor GroundsmanGreen = new UIColor(red: 0.30f, green: 0.69f, blue: 0.31f, alpha: 1.00f);
+        readonly UIColor GroundsmanGreen = new UIColor(red: 0.30f, green: 0.69f, blue: 0.31f, alpha: 1.00f);
         protected override void OnElementChanged(ElementChangedEventArgs<View> e)
         {
             base.OnElementChanged(e);
@@ -23,11 +22,52 @@ namespace Groundsman.iOS.Renderers
                 MKMapView nativeMap = Control as MKMapView;
                 nativeMap.ShowsCompass = false;
                 nativeMap.TintColor = GroundsmanGreen;
-                AddUserTrackingButton(nativeMap);
+
+                if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+                {
+                    AddToolbar(nativeMap);
+                }
+                else
+                {
+                    AddTrackingButton(nativeMap);
+                }
+
+                AddCompassButton(nativeMap);
             }
         }
 
-        private void AddUserTrackingButton(MKMapView nativeMap)
+        private void AddTrackingButton(MKMapView nativeMap)
+        {
+            MKUserTrackingButton button = MKUserTrackingButton.FromMapView(nativeMap);
+            button.Layer.BackgroundColor = UIColor.White.ColorWithAlpha(0.9F).CGColor;
+            button.Layer.BorderColor = UIColor.White.ColorWithAlpha(0.9F).CGColor;
+            button.Layer.ShadowColor = UIColor.Black.CGColor;
+            button.Layer.ShadowOpacity = 0.3F;
+            button.Layer.ShadowRadius = 4;
+            button.Layer.BorderWidth = 1;
+            button.Layer.CornerRadius = 4;
+            button.Layer.MasksToBounds = false;
+            button.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            nativeMap.AddSubview(button);
+            button.TopAnchor.ConstraintEqualTo(nativeMap.TopAnchor, 58).Active = true;
+            button.RightAnchor.ConstraintEqualTo(nativeMap.RightAnchor, -12).Active = true;
+        }
+
+        private void AddCompassButton(MKMapView nativeMap)
+        {
+            MKCompassButton compass = MKCompassButton.FromMapView(nativeMap);
+            compass.CompassVisibility = MKFeatureVisibility.Visible;
+            compass.Frame = new CGRect(new CGPoint(45, 45), compass.Frame.Size);
+            compass.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            nativeMap.AddSubview(compass);
+
+            compass.TopAnchor.ConstraintEqualTo(nativeMap.TopAnchor, 108).Active = true;
+            compass.RightAnchor.ConstraintEqualTo(nativeMap.RightAnchor, -14).Active = true;
+        }
+
+        private void AddToolbar(MKMapView nativeMap)
         {
             // Tracking button
             MKUserTrackingBarButtonItem trackingButton = new MKUserTrackingBarButtonItem(nativeMap);
@@ -45,15 +85,14 @@ namespace Groundsman.iOS.Renderers
             UIBarButtonItem flex = new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace, target: this, action: null);
 
             // Set toolbar items - can be expanded later
-            toolbar.Items = new UIBarButtonItem[] { flex, trackingButton, flex };
-
+            toolbar.SetItems(new UIBarButtonItem[] { flex, trackingButton, flex }, true);
             // Set toolbar position
             var origin = new CGPoint(x: UIScreen.MainScreen.Bounds.Width - 53, y: 52);
 
             // Create base view for button
             UIView baseView = new UIView(frame: new CGRect(location: origin, size: new CGSize(width: 44, height: 44)))
             {
-              BackgroundColor = UIColor.Clear
+                BackgroundColor = UIColor.Clear
             };
 
             // Shadow effects
@@ -67,8 +106,10 @@ namespace Groundsman.iOS.Renderers
             //baseView.Layer.RasterizationScale = UIScreen.MainScreen.Scale;
 
             // Rounded border view for button
-            UIView borderview = new UIView();
-            borderview.Frame = baseView.Bounds;
+            UIView borderview = new UIView
+            {
+                Frame = baseView.Bounds
+            };
             borderview.Layer.CornerRadius = 10;
             borderview.Layer.MasksToBounds = true;
 
@@ -76,17 +117,6 @@ namespace Groundsman.iOS.Renderers
             baseView.AddSubview(borderview);
             borderview.AddSubview(toolbar);
             nativeMap.AddSubview(baseView);
-
-            // Compass button
-            MKCompassButton compass = MKCompassButton.FromMapView(nativeMap);
-            compass.CompassVisibility = MKFeatureVisibility.Visible;
-            compass.Frame = new CGRect(new CGPoint(45, 45), compass.Frame.Size);
-            compass.TranslatesAutoresizingMaskIntoConstraints = false;
-
-            nativeMap.AddSubview(compass);
-
-            compass.TopAnchor.ConstraintEqualTo(nativeMap.TopAnchor, 108).Active = true;
-            compass.RightAnchor.ConstraintEqualTo(nativeMap.RightAnchor, -14).Active = true;
         }
     }
 }
