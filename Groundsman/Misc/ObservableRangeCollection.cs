026f68dc1a65a368ff561fa12040426e080a2ba9
ReillyM-C <41772129+ReillyM-C@users.noreply.github.com>
2021-01-06 14:36:44 +1000

    Update to Xamarin.Forms 5

--- Groundsman/Misc/ObservableRangeCollection.cs
+++ Groundsman/Misc/ObservableRangeCollection.cs
@@ -12,28 +12,27 @@ namespace Groundsman
     /// <typeparam name="T"></typeparam> 
     public class ObservableRangeCollection<T> : ObservableCollection<T>
     {
-
-        /// <summary> 
-        /// Initializes a new instance of the System.Collections.ObjectModel.ObservableCollection(Of T) class. 
-        /// </summary> 
+        /// <summary>
+        /// Initializes a new instance of the System.Collections.ObjectModel.ObservableCollection(Of T) class.
+        /// </summary>
         public ObservableRangeCollection()
             : base()
         {
         }
 
-        /// <summary> 
-        /// Initializes a new instance of the System.Collections.ObjectModel.ObservableCollection(Of T) class that contains elements copied from the specified collection. 
-        /// </summary> 
-        /// <param name="collection">collection: The collection from which the elements are copied.</param> 
-        /// <exception cref="System.ArgumentNullException">The collection parameter cannot be null.</exception> 
+        /// <summary>
+        /// Initializes a new instance of the System.Collections.ObjectModel.ObservableCollection(Of T) class that contains elements copied from the specified collection.
+        /// </summary>
+        /// <param name="collection">collection: The collection from which the elements are copied.</param>
+        /// <exception cref="System.ArgumentNullException">The collection parameter cannot be null.</exception>
         public ObservableRangeCollection(IEnumerable<T> collection)
             : base(collection)
         {
         }
 
-        /// <summary> 
-        /// Adds the elements of the specified collection to the end of the ObservableCollection(Of T). 
-        /// </summary> 
+        /// <summary>
+        /// Adds the elements of the specified collection to the end of the ObservableCollection(Of T).
+        /// </summary>
         public void AddRange(IEnumerable<T> collection, NotifyCollectionChangedAction notificationMode = NotifyCollectionChangedAction.Add)
         {
             if (notificationMode != NotifyCollectionChangedAction.Add && notificationMode != NotifyCollectionChangedAction.Reset)
@@ -53,11 +52,12 @@ namespace Groundsman
             if (notificationMode == NotifyCollectionChangedAction.Reset)
             {
                 RaiseChangeNotificationEvents(action: NotifyCollectionChangedAction.Reset);
-
                 return;
             }
 
-            var changedItems = collection is List<T> ? (List<T>)collection : new List<T>(collection);
+            var changedItems = collection is List<T>
+                ? (List<T>)collection
+                : new List<T>(collection);
 
             RaiseChangeNotificationEvents(
                 action: NotifyCollectionChangedAction.Add,
@@ -65,9 +65,9 @@ namespace Groundsman
                 startingIndex: startIndex);
         }
 
-        /// <summary> 
+        /// <summary>
         /// Removes the first occurence of each item in the specified collection from ObservableCollection(Of T). NOTE: with notificationMode = Remove, removed items starting index is not set because items are not guaranteed to be consecutive.
-        /// </summary> 
+        /// </summary>
         public void RemoveRange(IEnumerable<T> collection, NotifyCollectionChangedAction notificationMode = NotifyCollectionChangedAction.Reset)
         {
             if (notificationMode != NotifyCollectionChangedAction.Remove && notificationMode != NotifyCollectionChangedAction.Reset)
@@ -97,7 +97,7 @@ namespace Groundsman
             {
                 if (!Items.Remove(changedItems[i]))
                 {
-                    changedItems.RemoveAt(i); //Can't use a foreach because changedItems is intended to be (carefully) modified
+                    changedItems.RemoveAt(i); // Can't use a foreach because changedItems is intended to be (carefully) modified
                     i--;
                 }
             }
@@ -110,14 +110,14 @@ namespace Groundsman
                 changedItems: changedItems);
         }
 
-        /// <summary> 
-        /// Clears the current collection and replaces it with the specified item. 
-        /// </summary> 
+        /// <summary>
+        /// Clears the current collection and replaces it with the specified item.
+        /// </summary>
         public void Replace(T item) => ReplaceRange(new T[] { item });
 
-        /// <summary> 
-        /// Clears the current collection and replaces it with the specified collection. 
-        /// </summary> 
+        /// <summary>
+        /// Clears the current collection and replaces it with the specified collection.
+        /// </summary>
         public void ReplaceRange(IEnumerable<T> collection)
         {
             if (collection == null)
@@ -139,7 +139,7 @@ namespace Groundsman
             RaiseChangeNotificationEvents(action: NotifyCollectionChangedAction.Reset);
         }
 
-        private bool AddArrangeCore(IEnumerable<T> collection)
+        bool AddArrangeCore(IEnumerable<T> collection)
         {
             var itemAdded = false;
             foreach (var item in collection)
@@ -150,7 +150,7 @@ namespace Groundsman
             return itemAdded;
         }
 
-        private void RaiseChangeNotificationEvents(NotifyCollectionChangedAction action, List<T>? changedItems = null, int startingIndex = -1)
+        void RaiseChangeNotificationEvents(NotifyCollectionChangedAction action, List<T>? changedItems = null, int startingIndex = -1)
         {
             OnPropertyChanged(new PropertyChangedEventArgs(nameof(Count)));
             OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
